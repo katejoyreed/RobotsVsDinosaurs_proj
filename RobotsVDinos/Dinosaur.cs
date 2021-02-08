5f372288b2c1b0023b16baa2f408305ec4ad0180
katejoyreed <katejoyreed@gmail.com>
2021-02-08 11:29:57 -0600

    Added member variables and ctor to Dinosaur Class

--- RobotsVDinos/Dinosaur.cs
+++ RobotsVDinos/Dinosaur.cs
@@ -6,10 +6,19 @@ namespace RobotsVDinos
 {
     class Dinosaur
     {//member variables
-
+        string type;
+        double hitPoints;
+        double energy;
+        double attackPower;
 
         //ctor
-
+        public Dinosaur(string type, double hitPoints, double energy, double attackPower)
+        {
+            this.type = type;
+            this.hitPoints = hitPoints;
+            this.energy = energy;
+            this.attackPower = attackPower;
+        }
 
         //member methods
     }
