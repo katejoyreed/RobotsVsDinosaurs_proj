abb53ab32e83a2c5431e2b22ec3a7ec0a1cc4559
katejoyreed <katejoyreed@gmail.com>
2021-02-08 11:45:59 -0600

    Added member variables and ctor to Weapon Class

--- RobotsVDinos/Weapon.cs
+++ RobotsVDinos/Weapon.cs
@@ -6,10 +6,15 @@ namespace RobotsVDinos
 {
     class Weapon
     {//member variables
-
+        string type;
+        double attackPower;
 
         //ctor
-
+        public Weapon(string type, double attackPower)
+        {
+            this.type = type;
+            this.attackPower = attackPower;
+        }
 
         //member methods
     }
